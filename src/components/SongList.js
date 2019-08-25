8cd67d33e2edb0b57a4f0833066e813b5033ce75
olayeancarh <akejuolayinka016@gmail.com>
2019-08-25 15:41:53 +0100

    Minor update to SongList.js

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class SongList extends React.Component {
+class SongList extends Component {
   render() {
     return <div>SongList</div>;
   }
